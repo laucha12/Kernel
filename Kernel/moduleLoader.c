f818731274a8c65fa470d75c3cd714a53c53b1ea
marco444 <elscili444.code@gmail.com>
2022-05-18 14:37:31 -0300

    Cleaning modules

--- Kernel/moduleLoader.c
+++ Kernel/moduleLoader.c
@@ -5,6 +5,7 @@
 
 static void loadModule(uint8_t ** module, void * targetModuleAddress);
 static uint32_t readUint32(uint8_t ** address);
+
 //Comentario de prueba
 void loadModules(void * payloadStart, void ** targetModuleAddress)
 {
@@ -20,18 +21,8 @@ static void loadModule(uint8_t ** module, void * targetModuleAddress)
 {
 	uint32_t moduleSize = readUint32(module);
 
-	ncPrint("  Will copy module at 0x");
-	ncPrintHex((uint64_t)*module);
-	ncPrint(" to 0x");
-	ncPrintHex((uint64_t)targetModuleAddress);
-	ncPrint(" (");
-	ncPrintDec(moduleSize);
-	ncPrint(" bytes)");
-
 	memcpy(targetModuleAddress, *module, moduleSize);
 	*module += moduleSize;
-
-	ncPrint(" [Done]");
 	ncNewline();
 }
 
