e718732cefd42057a0f9e96fdd8ea49055246d5c
martinippo01 <martinippo@gmail.com>
2022-05-20 19:37:38 -0300

    Working on time syscall

--- Kernel/irqDispatcher.c
+++ Kernel/irqDispatcher.c
@@ -2,6 +2,7 @@
 #include <stdint.h>
 #include <keyBoard.h>
 #include <naiveConsole.h>
+#include <lib.h>
 
 #define MAXBUFFER 255
 
@@ -44,7 +45,7 @@ void syscalls(int fd,char * sysBuffer,int count,int num){
 		{
 		
 		case 120:
-			
+			time_syscall(sysBuffer);
 			break;
 
 		case 1:
@@ -85,16 +86,67 @@ extern int get_rtc_day();
 extern int  get_rtc_month();
 extern int get_rtc_year();
 
-void time_syscall(){
-	int seconds = get_rtc_seconds();
-	int minutes = get_rtc_minutes();
-	int hours = get_rtc_hours();
-	int weekday = get_rtc_weekday();
-	int day = get_rtc_day();
-	int month = get_rtc_month();
-	int year = get_rtc_year();
-	
+int bcd_decimal(uint8_t hex)
+{
+	// sacar los assert!
+    assert(((hex & 0xF0) >> 4) < 10);  // More significant nybble is valid
+    assert((hex & 0x0F) < 10);         // Less significant nybble is valid
+    int dec = ((hex & 0xF0) >> 4) * 10 + (hex & 0x0F);
+    return dec;
+}     
 
+void convert_weekday(char * buffer, int weekday){
+	switch(weekday){
+		case 1:
+			buffer = "MON";
+			break;
+		case 2:
+			buffer = "TUE";
+			break;
+		case 3:
+			buffer = "WED";
+			break;
+		case 4:
+			buffer = "THR";
+			break;
+		case 5:
+			buffer = "FRI";
+			break;
+		case 6:
+			buffer = "SAT";
+			break;
+		case 7:
+			buffer = "SUN";
+			break;
+		default:
+			buffer = "---";
+			break;
+	}
+}
+
+void time_syscall(char * buffer){
+	int seconds = bcd_decimal(get_rtc_seconds());
+	int minutes = bcd_decimal(get_rtc_minutes());
+	int hours = bcd_decimal(get_rtc_hours());
+	int weekday = bcd_decimal(get_rtc_weekday());
+	int day = bcd_decimal(get_rtc_day());
+	int month = bcd_decimal(get_rtc_month());
+	int year = bcd_decimal(get_rtc_year());
+	
+	convert_weekday(buffer, weekday);
+	buffer[3] = ' ';
+	itoa(buffer + 4, day, 2);
+	buffer[6] = '/';
+	itoa(buffer + 7, month, 2);
+	buffer[9] = '/';
+	itoa(buffer + 10, year, 4);
+	buffer[14] = ' ';
+	itoa(buffer + 15, hours, 2);
+	buffer[17] = ':';
+	itoa(buffer + 18, minutes, 2);
+	buffer[20] = ':';
+	itoa(buffer + 21, seconds, 2);
+	buffer[23] = '\0';
 
 }
 
