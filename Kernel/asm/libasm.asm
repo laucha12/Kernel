008085dd607e6a9c9d3eec6e147cc8966f564a87
martinippo01 <martinippo@gmail.com>
2022-05-20 18:46:56 -0300

    Minor changes

--- Kernel/asm/libasm.asm
+++ Kernel/asm/libasm.asm
@@ -1,5 +1,13 @@
 GLOBAL cpuVendor
 GLOBAL readKey
+GLOBAL get_rtc_seconds
+GLOBAL get_rtc_minutes
+GLOBAL get_rtc_hours
+GLOBAL get_rtc_day
+GLOBAL get_rtc_weekday
+GLOBAL get_rtc_month
+GLOBAL get_rtc_year
+
 section .text
 	
 cpuVendor:
@@ -47,3 +55,195 @@ readKey:
 	mov rsp,rbp
 	pop rbp
 	ret
+
+	;---------------------------------------------------
+;	Funcion la cual retorna los segundos del sistema
+;	mediante el rtc (real time clock)
+;---------------------------------------------------
+;	Argumentos: -
+;---------------------------------------------------
+;	Retorno: Segundos del sistema (en rax)
+;---------------------------------------------------
+get_rtc_seconds:
+    push rbp        ; Stack frame
+    mov rbp, rsp    ; Stack frame
+
+    push rbx        ; Preservar rbx
+
+    mov rax, 0      ; Lleno de 0 el rax
+
+    mov al, 0       ; Indico el registro de rtc
+    out 70h, al     ; Indico el registro de rtc
+    in al, 71h      ; Leo el rtc
+
+    pop rbx         ; Preservar rbx
+    
+    mov rsp, rbp   ; Stack frame
+	pop rbp         ; Stack frame
+
+    ret
+
+
+;---------------------------------------------------
+;	Funcion la cual retorna los minutos del sistema
+;	mediante el rtc (real time clock)
+;---------------------------------------------------
+;	Argumentos: - 
+;---------------------------------------------------
+;	Retorno: Minutos del sistema (en rax)
+;---------------------------------------------------
+get_rtc_minutes:
+    push rbp        ; Stack frame
+    mov rbp, rsp    ; Stack frame
+
+    push rbx        ; Preservar rbx
+
+    mov rax, 0      ; Lleno de 0 el rax
+
+    mov al, 0x2       ; Indico el registro de rtc
+    out 70h, al     ; Indico el registro de rtc
+    in al, 71h      ; Leo el rtc
+
+    pop rbx         ; Preservar rbx
+    
+    mov rsp, rbp   ; Stack frame
+	pop rbp         ; Stack frame
+
+    ret
+
+
+;---------------------------------------------------
+;	Funcion la cual retorna los segundos del sistema
+;	mediante el rtc (real time clock)
+;---------------------------------------------------
+;	Argumentos: -
+;---------------------------------------------------
+;	Retorno: Hora del sistema (en rax)
+;---------------------------------------------------
+get_rtc_hours:
+    push rbp        ; Stack frame
+    mov rbp, rsp    ; Stack frame
+
+    push rbx        ; Preservar rbx
+
+    mov rax, 0      ; Lleno de 0 el rax
+
+    mov al, 0x4       ; Indico el registro de rtc
+    out 70h, al     ; Indico el registro de rtc
+    in al, 71h      ; Leo el rtc
+
+    pop rbx         ; Preservar rbx
+    
+    mov rsp, rbp   ; Stack frame
+	pop rbp         ; Stack frame
+
+    ret
+
+;---------------------------------------------------
+;	Funcion la cual retorna el dia del sistema
+;	mediante el rtc (real time clock)
+;---------------------------------------------------
+;	Argumentos: -
+;---------------------------------------------------
+;	Retorno: Segundos del sistema (en rax)
+;---------------------------------------------------
+get_rtc_day:
+    push rbp        ; Stack frame
+    mov rbp, rsp    ; Stack frame
+
+    push rbx        ; Preservar rbx
+
+    mov rax, 0      ; Lleno de 0 el rax
+
+    mov al, 0x07    ; Indico el registro de rtc
+    out 70h, al     ; Indico el registro de rtc
+    in al, 71h      ; Leo el rtc
+
+    pop rbx         ; Preservar rbx
+    
+    mov rsp, rbp   ; Stack frame
+	pop rbp         ; Stack frame
+
+    ret
+
+;---------------------------------------------------
+;	Funcion la cual retorna el dia de la semana 
+;	del sistema mediante el rtc (real time clock)
+;---------------------------------------------------
+;	Argumentos: -
+;---------------------------------------------------
+;	Retorno: Segundos del sistema (en rax)
+;---------------------------------------------------
+get_rtc_weekday:
+    push rbp        ; Stack frame
+    mov rbp, rsp    ; Stack frame
+
+    push rbx        ; Preservar rbx
+
+    mov rax, 0      ; Lleno de 0 el rax
+
+    mov al, 0x07    ; Indico el registro de rtc
+    out 70h, al     ; Indico el registro de rtc
+    in al, 71h      ; Leo el rtc
+
+    pop rbx         ; Preservar rbx
+    
+    mov rsp, rbp   ; Stack frame
+	pop rbp         ; Stack frame
+
+    ret
+
+;---------------------------------------------------
+;	Funcion la cual retorna el mes del sistema
+;	mediante el rtc (real time clock)
+;---------------------------------------------------
+;	Argumentos: -
+;---------------------------------------------------
+;	Retorno: Segundos del sistema (en rax)
+;---------------------------------------------------
+get_rtc_month:
+    push rbp        ; Stack frame
+    mov rbp, rsp    ; Stack frame
+
+    push rbx        ; Preservar rbx
+
+    mov rax, 0      ; Lleno de 0 el rax
+
+    mov al, 0x8     ; Indico el registro de rtc
+    out 70h, al     ; Indico el registro de rtc
+    in al, 71h      ; Leo el rtc
+
+    pop rbx         ; Preservar rbx
+    
+    mov rsp, rbp   ; Stack frame
+	pop rbp         ; Stack frame
+
+    ret
+
+;---------------------------------------------------
+;	Funcion la cual retorna el anio del sistema
+;	mediante el rtc (real time clock)
+;---------------------------------------------------
+;	Argumentos: -
+;---------------------------------------------------
+;	Retorno: Segundos del sistema (en rax)
+;---------------------------------------------------
+get_rtc_year:
+    push rbp        ; Stack frame
+    mov rbp, rsp    ; Stack frame
+
+    push rbx        ; Preservar rbx
+
+    mov rax, 0      ; Lleno de 0 el rax
+
+    mov al, 0x9    ; Indico el registro de rtc
+    out 70h, al     ; Indico el registro de rtc
+    in al, 71h      ; Leo el rtc
+
+    pop rbx         ; Preservar rbx
+    
+    mov rsp, rbp   ; Stack frame
+	pop rbp         ; Stack frame
+
+    ret
+
