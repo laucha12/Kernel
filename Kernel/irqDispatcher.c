28f7b37707fce74905efee3b50a3d282f0aba591
laucha12 <lautihernando@gmail.com>
2022-05-21 14:43:01 -0300

    Modulies Kernel space

--- Kernel/irqDispatcher.c
+++ Kernel/irqDispatcher.c
@@ -1,17 +1,10 @@
-#include <time.h>
 #include <stdint.h>
 #include <keyBoard.h>
 #include <naiveConsole.h>
 #include <lib.h>
-
-#define MAXBUFFER 255
-
+#include <timeDriver.h>
 static void int_20();
 static void int_21();
-
-static char buffer [MAXBUFFER] = {0};
-static unsigned int size = 0;
-static unsigned int actualPos = 0;
 
 void irqDispatcher(uint64_t irq) {
 
@@ -30,8 +23,7 @@ void int_21(){
 	char c = readKey();
 	if (c > 0)
 	{
-		buffer[size] = scancodeLToAscii[c];
-		size = (size == 254)? 0 : size++;
+		saveBuffer(c);
 	}
 }
 
@@ -39,95 +31,6 @@ void int_20() {
 	//timer_handler();
 }
 
-
-// Funciones de libasm.asm
-extern int get_rtc_seconds();
-extern int get_rtc_minutes();
-extern int get_rtc_hours();
-extern int get_rtc_weekday();
-extern int get_rtc_day();
-extern int get_rtc_month();
-extern int get_rtc_year();
-
-int bcd_decimal(uint8_t hex)
-{
-	// sacar los assert!
-    (((hex & 0xF0) >> 4) < 10);  // More significant nybble is valid
-    ((hex & 0x0F) < 10);         // Less significant nybble is valid
-    int dec = ((hex & 0xF0) >> 4) * 10 + (hex & 0x0F);
-    return dec;
-}     
-
-void convert_weekday(char * buffer, int weekday){
-	switch(weekday){
-		case 1:
-			buffer[0] = 'M';
-			buffer[1] = 'O';
-			buffer[2] = 'N';
-			break;
-		case 2:
-			buffer[0] = 'T';
-			buffer[1] = 'U';
-			buffer[2] = 'E';
-			break;
-		case 3:
-			buffer[0] = 'W';
-			buffer[1] = 'E';
-			buffer[2] = 'D';
-			break;
-		case 4:
-			buffer[0] = 'T';
-			buffer[1] = 'H';
-			buffer[2] = 'R';
-			break;
-		case 5:
-			buffer[0] = 'F';
-			buffer[1] = 'R';
-			buffer[2] = 'I';
-			break;
-		case 6:
-			buffer[0] = 'S';
-			buffer[1] = 'A';
-			buffer[2] = 'T';
-			break;
-		case 7:
-			buffer[0] = 'S';
-			buffer[1] = 'U';
-			buffer[2] = 'N';
-			break;
-		default:
-			buffer[0] = '-';
-			buffer[1] = '-';
-			buffer[2] = '-';
-			break;
-	}
-}
-
-void time_syscall(char * buffer){
-	int seconds = bcd_decimal(get_rtc_seconds());
-	int minutes = bcd_decimal(get_rtc_minutes());
-	int hours = bcd_decimal(get_rtc_hours());
-	int weekday = bcd_decimal(get_rtc_weekday());
-	int day = bcd_decimal(get_rtc_day());
-	int month = bcd_decimal(get_rtc_month());
-	int year = bcd_decimal(get_rtc_year());
-	
-	convert_weekday(buffer, weekday);
-	buffer[3] = ' ';
-	itoa(buffer + 4, day, 2);
-	buffer[6] = '/';
-	itoa(buffer + 7, month, 2);
-	buffer[9] = '/';
-	itoa(buffer + 10, year, 4);
-	buffer[14] = ' ';
-	itoa(buffer + 15, hours, 2);
-	buffer[17] = ':';
-	itoa(buffer + 18, minutes, 2);
-	buffer[20] = ':';
-	itoa(buffer + 21, seconds, 2);
-	buffer[23] = '\0';
-
-}
 
 void syscalls(int fd,char * sysBuffer,int count,int num){
 
@@ -155,11 +58,7 @@ void syscalls(int fd,char * sysBuffer,int count,int num){
 				
 			break;
 		case 0:
-			sysBuffer[0] = buffer[actualPos];
-			if(buffer[actualPos] != 0){
-				buffer[actualPos] = 0;
-				actualPos = (actualPos == 254)? 0 : actualPos++;
-			}
+			getBufferChar(sysBuffer);
 			break;
 		default:
 			break;
